ffc96264251b60da56223c591977ce67d31ad6cd
Haydart <makowiecki.radoslaw@gmail.com>
2017-12-15 19:15:18 +0100

    Print coins list

--- detection/test_cronjob.py
+++ detection/test_cronjob.py
@@ -1,19 +1,16 @@
 import time
-import datetime
 import requests
 
 
 def crawl_bittrex():
     bittrex_market_response = requests.get(
         "https://bittrex.com/api/v1.1/public/getmarketsummaries")
-    bittrex_coins_json = bittrex_market_response.json()
-    print(bittrex_coins_json)
-    return bittrex_coins_json
+    return bittrex_market_response.json()
 
 
 while True:
-    print(datetime.datetime.now().time())
-    print(time.time())
-    time.sleep(5)
+    print('current time is ' + str(time.time()))
+    time.sleep(2)
 
-    crawl_bittrex()
+    for coin in crawl_bittrex()["result"]:
+        print(coin["MarketName"] + ' {:.8f}'.format(coin["Last"]))
