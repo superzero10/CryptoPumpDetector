671efa0438ebbfc391785b5047ad58a9ba970f09
Haydart <makowiecki.radoslaw@gmail.com>
2017-12-15 18:06:36 +0100

    Draw data from Bittrex API

--- detection/test_cronjob.py
+++ detection/test_cronjob.py
@@ -1,7 +1,19 @@
 import time
 import datetime
+import requests
+
+
+def crawl_bittrex():
+    bittrex_market_response = requests.get(
+        "https://bittrex.com/api/v1.1/public/getmarketsummaries")
+    bittrex_coins_json = bittrex_market_response.json()
+    print(bittrex_coins_json)
+    return bittrex_coins_json
+
 
 while True:
     print(datetime.datetime.now().time())
     print(time.time())
-    time.sleep(1)
+    time.sleep(5)
+
+    crawl_bittrex()
