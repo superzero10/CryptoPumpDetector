8b0e63b9b943c1580cecae2cbeefa655b7fee807
Haydart <makowiecki.radoslaw@gmail.com>
2017-12-15 23:37:13 +0100

    Add database connection

--- detection/detector.py
+++ detection/detector.py
@@ -1,16 +1,17 @@
 import time
 
-from detection.bittrex_cron_crawler import BittrexCrawler
+from detection.bittrex_cron_crawler import crawl_bittrex, crawl_active_btc_pairs
+from detection.constants import SERVER_SCRAPPING_FREQUENCY_SEC
 
-coin_data = BittrexCrawler.crawl_bittrex()
-active_coin_names = BittrexCrawler.crawl_active_btc_pairs()
+coin_data = crawl_bittrex()
+active_coin_names = crawl_active_btc_pairs()
 print(active_coin_names)
 
 while True:
-    time.sleep(15)
+    time.sleep(SERVER_SCRAPPING_FREQUENCY_SEC)
     print('current time is ' + str(time.time()))
 
-    coin_data = BittrexCrawler.crawl_bittrex()
+    coin_data = crawl_bittrex()
     for coin in coin_data["result"]:
         if str(coin["MarketName"]).startswith('BTC'):
             print(coin["MarketName"] + ' {:.8f}'.format(coin["Last"]))
