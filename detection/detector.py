030fa340306970ca12d86c0a623686c6224c4024
Haydart <makowiecki.radoslaw@gmail.com>
2017-12-16 23:15:28 +0100

    Propose possible pumping coins

--- detection/detector.py
+++ detection/detector.py
@@ -1,7 +1,7 @@
 import time
 import datetime
 from detection.bittrex_service import BittrexService
-from detection.constants import SERVER_REQUEST_FREQUENCY_SEC, MIN_BTC_VOLUME
+from detection.constants import SERVER_REQUEST_FREQUENCY_SEC, MIN_BTC_VOLUME, MIN_SOAR_THRESHOLD
 from detection.database_connection import obtain_db_connection
 
 wanted_keys = {'MarketName', 'BaseVolume', 'Bid', 'Ask', 'OpenBuyOrders', 'OpenSellOrders'}
@@ -19,15 +19,21 @@ while True:
 
     coin_data = new_coin_data
     new_coin_data = bittrex_service.fetch_coin_data()
-    for coin in new_coin_data["result"]:
+    for coin in new_coin_data['result']:
         if str(coin['MarketName']).startswith('BTC') and coin['BaseVolume'] >= MIN_BTC_VOLUME:
-            unwanted_keys = set(coin.keys()) - wanted_keys
-            for unwanted_key in unwanted_keys:
-                del coin[unwanted_key]
-            print(coin)
+            old_coin = next((item for item in coin_data['result'] if item['MarketName'] == coin['MarketName']))
+            # print(old_coin)
+            if coin['Ask'] >= old_coin['Ask'] * MIN_SOAR_THRESHOLD:
+                print('Possible pump coin: ', old_coin['MarketName'], ', was: ', old_coin['Ask'], ', is: ', coin['Ask'])
 
-    # db_cursor = connection.cursor()
-    # db_cursor.execute('SELECT * FROM COMPANY;')
-    # rows = db_cursor.fetchall()
-    # for row in rows:
-    #     print(row[0], row[1], )2
+                unwanted_keys = set(coin.keys()) - wanted_keys
+                for unwanted_key in unwanted_keys:
+                    del coin[unwanted_key]
+
+    print('')
+
+                # db_cursor = connection.cursor()
+                # db_cursor.execute('SELECT * FROM COMPANY;')
+                # rows = db_cursor.fetchall()
+                # for row in rows:
+                #     print(row[0], row[1], )2
