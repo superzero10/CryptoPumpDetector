b19d248d18ea71b7f9f9e97598bbe10ec06b0b2d
Haydart <makowiecki.radoslaw@gmail.com>
2017-12-16 09:03:13 +0100

    Use 'Ask' rather than 'Last'

--- detection/detector.py
+++ detection/detector.py
@@ -1,10 +1,13 @@
+import os
 import time
 
 from detection.bittrex_cron_crawler import crawl_bittrex, crawl_active_btc_pairs
-from detection.constants import SERVER_SCRAPPING_FREQUENCY_SEC
+from detection.constants import SERVER_SCRAPPING_FREQUENCY_SEC, MIN_BTC_VOLUME
+from detection.database_connection import connect_to_database
 
 coin_data = crawl_bittrex()
 active_coin_names = crawl_active_btc_pairs()
+connect_to_database()
 print(active_coin_names)
 
 while True:
@@ -13,7 +16,7 @@ while True:
 
     coin_data = crawl_bittrex()
     for coin in coin_data["result"]:
-        if str(coin["MarketName"]).startswith('BTC'):
-            print(coin["MarketName"] + ' {:.8f}'.format(coin["Last"]))
+        if str(coin["MarketName"]).startswith('BTC') and coin["BaseVolume"] >= MIN_BTC_VOLUME:
+            print(coin["MarketName"] + ' {:.8f}'.format(coin["Ask"]))
 
     print('')
