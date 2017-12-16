13c1458c5adb7c88e4f367fb1915c2bc1e21423a
Haydart <makowiecki.radoslaw@gmail.com>
2017-12-16 09:02:10 +0100

    Hardcode the database connection

--- detection/database_connection.py
+++ detection/database_connection.py
@@ -3,12 +3,20 @@ from urllib import parse
 import psycopg2
 
 parse.uses_netloc.append("postgres")
-url = parse.urlparse(os.environ["DATABASE_URL"])
+url = parse.urlparse('pumpdetectordb')
 
-connection = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+
+def connect_to_database():
+    connection = psycopg2.connect(
+        database=url.path,
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    db_cursor = connection.cursor()
+    db_cursor.execute('SELECT * FROM COMPANY;')
+    rows = db_cursor.fetchall()
+    for row in rows:
+        print(" ", row[0], " ", row[1])
+    return connection
