4ac8c19caf7d12e18eb2a3889418058a6c6f2df1
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-06 19:51:19 +0100

    Distinguish between fresh and remembered state of the fetched pump groups

--- telegram_pumps/database/database_retriever.py
+++ telegram_pumps/database/database_retriever.py
@@ -1,2 +1,34 @@
+from common.database.database_connection import obtain_db_connection
 
-def fetch_all_groups():
+
+def fetch_all_groups(fresh_state_needed):
+    groups_list = []
+
+    if fresh_state_needed:
+        db_connection = obtain_db_connection()
+        db_cursor = db_connection.cursor()
+        db_cursor.execute('SELECT group_id, signal_type FROM pump_groups')
+        groups_list = db_cursor.fetchall()
+        db_connection.close()
+    else:
+        if not all_groups_list:
+            groups_list = fetch_all_groups(True)
+        else:
+            groups_list = all_groups_list
+
+    return groups_list
+
+
+all_groups_list = fetch_all_groups(True)
+
+
+def fetch_text_signal_groups():
+    pass
+
+
+def fetch_image_signal_groups():
+    pass
+
+
+def fetch_unknown_signal_type_groups():
+    pass
--- telegram_pumps/unknown_tracing/unknown_group_message_tracer.py
+++ telegram_pumps/unknown_tracing/unknown_group_message_tracer.py
@@ -1,13 +1,5 @@
 from common.database.database_connection import obtain_db_connection
 
-db_connection = obtain_db_connection()
-db_cursor = db_connection.cursor()
-db_cursor.execute('SELECT group_id, signal_type FROM pump_groups')
-all_groups_list = db_cursor.fetchall()
-print(all_groups_list)
-
-db_connection.close()
-
 
 def trace_unknown_group_messages(full_message):
     pass
