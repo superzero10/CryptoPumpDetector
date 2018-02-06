bac1a14a2cfa99bc45594e218233ca519a8f9540
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-06 20:48:22 +0100

    Implement pump group data cached fetching from db

--- telegram_pumps/database/database_retriever.py
+++ telegram_pumps/database/database_retriever.py
@@ -1,4 +1,5 @@
 from common.database.database_connection import obtain_db_connection
+from time import time
 
 
 def fetch_all_groups(fresh_state_needed):
@@ -19,16 +20,24 @@ def fetch_all_groups(fresh_state_needed):
     return groups_list
 
 
+def fetch_text_signal_groups(fresh_state_needed):
+    return text_groups_list if not fresh_state_needed and text_groups_list else \
+        [group[0] for group in fetch_all_groups(fresh_state_needed) if group[1] == 'text']
+
+
+def fetch_image_signal_groups(fresh_state_needed):
+    return image_groups_list if not fresh_state_needed and image_groups_list else \
+        [group[0] for group in fetch_all_groups(fresh_state_needed) if group[1] == 'image']
+
+
+def fetch_unknown_signal_type_groups(fresh_state_needed):
+    return unknown_groups_list if not fresh_state_needed and unknown_groups_list else \
+        [group[0] for group in fetch_all_groups(fresh_state_needed) if group[1] == 'unknown']
+
+
 all_groups_list = fetch_all_groups(True)
+text_groups_list = fetch_text_signal_groups(True)
+image_groups_list = fetch_image_signal_groups(True)
+unknown_groups_list = fetch_unknown_signal_type_groups(True)
 
-
-def fetch_text_signal_groups():
-    pass
-
-
-def fetch_image_signal_groups():
-    pass
-
-
-def fetch_unknown_signal_type_groups():
-    pass
+print(fetch_text_signal_groups(False))
