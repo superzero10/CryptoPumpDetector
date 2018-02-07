b0d61216ad4ca4f4a84ab7fa338d55eecd39df1a
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-08 00:54:20 +0100

    Read a hardcoded auth code from file

--- telegram_pumps/detection/telegram_message_api_stream.py
+++ telegram_pumps/detection/telegram_message_api_stream.py
@@ -22,7 +22,7 @@ def initialize_client():
         client.send_code_request(user_phone)
         code_ok = False
         while not code_ok:
-            code = 1234
+            code = open('auth_code.txt').read()
             try:
                 code_ok = client.sign_in(user_phone, code)
             except SessionPasswordNeededError:
