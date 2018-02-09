d5ed4ea0e25b4f2e8cc1712ee63fa33d64b2a96e
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-09 11:04:01 +0100

    Make database connection with both local & remote database

--- common/database/database_connection.py
+++ common/database/database_connection.py
@@ -1,49 +1,22 @@
 import os
-from urllib import parse
-import psycopg2
-from psycopg2._psycopg import OperationalError
 import urllib.parse as urlparse
+from urllib import parse
 
+import psycopg2
 
 parse.uses_netloc.append('postgres')
 db_name = os.environ.get('DATABASE_URL')
 
 
 def create_db_connection():
-    # if db_name is None:
-    #     url = parse.urlparse('pumpdetectordb')
-    #     url_path = url.path
-    # else:
-    #     url = parse.urlparse('postgres-triangular-82542')
-    #     url_path = url.path
-    #
-    # print(url_path)
-    #
-    # try:
-    #     connection = psycopg2.connect(
-    #         database=url_path,
-    #         user=url.username,
-    #         password=url.password,
-    #         host=url.hostname,
-    #         port=url.port
-    #     )
-    # except OperationalError:
-    #     connection = psycopg2.connect(
-    #         database=url_path,
-    #         user=url.username,
-    #         password=url.password,
-    #         host="/tmp",
-    #         port=url.port
-    #     )
-
     url = urlparse.urlparse(os.environ['DATABASE_URL'])
-    dbname = url.path[1:]
+    dbname = url.path[1:] if url.path != 'pumpdetectordb' else url.path
     user = url.username
     password = url.password
     host = url.hostname
     port = url.port
 
-    con = psycopg2.connect(
+    connection = psycopg2.connect(
         dbname=dbname,
         user=user,
         password=password,
@@ -51,6 +24,7 @@ def create_db_connection():
         port=port
     )
 
-    return con
+    return connection
+
 
 create_db_connection()
