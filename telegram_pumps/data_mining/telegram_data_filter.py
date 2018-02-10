6c7461df446a9850f5573ff3406837a59b7f8bd6
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-10 23:13:53 +0100

    Revert breaking refactor

--- telegram_pumps/data_mining/telegram_data_filter.py
+++ telegram_pumps/data_mining/telegram_data_filter.py
@@ -1,5 +1,3 @@
-import time
-
 from telegram_pumps.database.database_retriever import *
 from telegram_pumps.database.database_writer import save_unknown_group_message, save_unlisted_group
 
@@ -8,9 +6,6 @@ text_signal_groups = fetch_text_signal_groups(True)
 image_signal_groups = fetch_image_signal_groups(True)
 unknown_signal_groups = fetch_unknown_signal_groups(True)
 
-keys_to_remove = ['fwd_from', 'date', 'to_id', 'media_unread', 'out', 'mentioned', 'via_bot_id', 'reply_to_msg_id',
-                  'id', 'edit_date', 'post_author', 'views', 'from_id', 'entities']
-
 
 def handle_data_updates(message):
     group_id = message.to_id.channel_id
@@ -18,7 +13,7 @@ def handle_data_updates(message):
     if group_id not in all_groups_id_list:
         print('- Message from non-listed group, saving message to db..')
         save_unlisted_group(group_id)
-        save_unknown_group_message(cleaned_message=_clean_message(message))
+        save_unknown_group_message(message)
 
     if group_id in unknown_signal_groups:
         print('- Message from a group whose signal type is unknown, saving message to db..')
@@ -29,15 +24,6 @@ def handle_data_updates(message):
 
     if group_id in image_signal_groups:
         _process_image_signal_group_message(message)
-
-
-def _clean_message(full_message):
-    message_dict = full_message.to_dict(recursive=True)
-
-    if not isinstance(message_dict, dict):
-        return message_dict
-    return {key: value for key, value in ((key, _clean_message(value)) for key, value in message_dict.items()) if
-            key not in keys_to_remove}
 
 
 def _process_text_signal_group_message(message):
--- telegram_pumps/database/database_writer.py
+++ telegram_pumps/database/database_writer.py
@@ -2,20 +2,34 @@ from common.database.database_connection import create_db_connection
 from psycopg2._json import Json
 from time import time
 
+keys_to_remove = ['fwd_from', 'date', 'to_id', 'media_unread', 'out', 'mentioned', 'via_bot_id', 'reply_to_msg_id',
+                  'id', 'edit_date', 'post_author', 'views', 'from_id', 'entities']
+
 
 def save_unknown_group_message(cleaned_message):
-    print('Message after cleaning', cleaned_message)
+    message_dict = cleaned_message.to_dict(recursive=True)
+
     db_connection = create_db_connection()
 
     try:
+        cleaned_message_dict = _clean_message(message_dict)
+        print('Message after cleaning', message_dict, '\n')
+
         db_cursor = db_connection.cursor()
         db_cursor.execute('INSERT into traced_messages (timestamp, chat_id, full_message) values (%s, %s, %s)',
-                          [time(), cleaned_message.to_id.channel_id, Json(cleaned_message)])
+                          [time(), cleaned_message.to_id.channel_id, Json(cleaned_message_dict)])
     except Exception as err:
         print(err)
     finally:
         db_connection.commit()
         db_connection.close()
+
+
+def _clean_message(message_dict):
+    if not isinstance(message_dict, dict):
+        return message_dict
+    return {key: value for key, value in ((key, _clean_message(value)) for key, value in message_dict.items()) if
+            key not in keys_to_remove}
 
 
 def save_unlisted_group(group_id):
