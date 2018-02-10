9942d09626305532fc42ac7de822aa8a2d0ed12d
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-10 20:52:46 +0100

    Fix message channel signal type recognition

--- telegram_pumps/data_mining/telegram_data_filter.py
+++ telegram_pumps/data_mining/telegram_data_filter.py
@@ -1,7 +1,7 @@
 from telegram_pumps.database.database_retriever import *
 from telegram_pumps.unknown_tracing.unknown_group_message_tracer import save_unknown_group_message
 
-all_groups_id_list = fetch_all_groups(True)
+all_groups_id_list = [group[0] for group in fetch_all_group_ids(True)]
 text_signal_groups = fetch_text_signal_groups(True)
 image_signal_groups = fetch_image_signal_groups(True)
 unknown_signal_groups = fetch_unknown_signal_groups(True)
@@ -11,9 +11,13 @@ def handle_data_updates(message):
     print(message)
     message_channel_id = message.to_id.channel_id
 
-    if message_channel_id not in all_groups_id_list or message_channel_id in unknown_signal_groups:
+    if message_channel_id not in all_groups_id_list:
         save_unknown_group_message(message)
-        print('Message from non-listed group or from a group whose signal type is unknown, saving message to db..')
+        print('- Message from non-listed group, saving message to db..')
+
+    if message_channel_id in unknown_signal_groups:
+        save_unknown_group_message(message)
+        print('- Message from a group whose signal type is unknown, saving message to db..')
 
     if message_channel_id in text_signal_groups:
         _process_text_signal_group_message(message)
@@ -23,8 +27,8 @@ def handle_data_updates(message):
 
 
 def _process_text_signal_group_message(message):
-    print('Processing a message from a text signal group')
+    print('- Message from a text signal group')
 
 
 def _process_image_signal_group_message(message):
-    print('Processing a message from an image signal group')
+    print('- Message from an image signal group')
--- telegram_pumps/database/database_retriever.py
+++ telegram_pumps/database/database_retriever.py
@@ -1,7 +1,7 @@
 from common.database.database_connection import create_db_connection
 
 
-def fetch_all_groups(fresh_state_needed):
+def fetch_all_group_ids(fresh_state_needed):
     groups_list = []
 
     if fresh_state_needed:
@@ -12,7 +12,7 @@ def fetch_all_groups(fresh_state_needed):
         db_connection.close()
     else:
         if not _all_groups_list:
-            groups_list = fetch_all_groups(True)
+            groups_list = fetch_all_group_ids(True)
         else:
             groups_list = _all_groups_list
 
@@ -21,20 +21,20 @@ def fetch_all_groups(fresh_state_needed):
 
 def fetch_text_signal_groups(fresh_state_needed):
     return _text_groups_list if not fresh_state_needed and _text_groups_list else \
-        [group[0] for group in fetch_all_groups(fresh_state_needed) if group[1] == 'text']
+        [group[0] for group in fetch_all_group_ids(fresh_state_needed) if group[1] == 'text']
 
 
 def fetch_image_signal_groups(fresh_state_needed):
     return _image_groups_list if not fresh_state_needed and _image_groups_list else \
-        [group[0] for group in fetch_all_groups(fresh_state_needed) if group[1] == 'image']
+        [group[0] for group in fetch_all_group_ids(fresh_state_needed) if group[1] == 'image']
 
 
 def fetch_unknown_signal_groups(fresh_state_needed):
     return _unknown_groups_list if not fresh_state_needed and _unknown_groups_list else \
-        [group[0] for group in fetch_all_groups(fresh_state_needed) if group[1] == 'unknown']
+        [group[0] for group in fetch_all_group_ids(fresh_state_needed) if group[1] == 'unknown']
 
 
-_all_groups_list = fetch_all_groups(True)
+_all_groups_list = fetch_all_group_ids(True)
 _text_groups_list = fetch_text_signal_groups(True)
 _image_groups_list = fetch_image_signal_groups(True)
 _unknown_groups_list = fetch_unknown_signal_groups(True)
