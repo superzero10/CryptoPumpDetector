9c852737eb19d1bef8153d1bc186952c086391e3
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-11 22:22:32 +0100

    Create class for keeping the list of recent trades

--- telegram_pumps/pump_coin_extraction/signal_message_recognition.py
+++ telegram_pumps/pump_coin_extraction/signal_message_recognition.py
@@ -2,5 +2,11 @@
 
 class PumpCoinRecognizer:
 
+    coinexchange_coins = []
+    yobit_coins = []
+    cryptopia_coins = []
+
+    trade_queue = TradeMemoryQueue()
+
     def __init__(self):
         pass
