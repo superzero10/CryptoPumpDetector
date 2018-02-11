51f2ba488c5cd6d2105103ff847a6561f7b187a0
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-11 22:38:03 +0100

    Add skeleton methods for checking if message is a pump or pump time announcement

--- telegram_pumps/pump_coin_extraction/signal_message_recognition.py
+++ telegram_pumps/pump_coin_extraction/signal_message_recognition.py
@@ -1,8 +1,15 @@
+import re
 
 
 class PumpCoinRecognizer:
 
-    trade_queue = TradeMemoryQueue()
-
-    def __init__(self):
+    def is_probable_pump_signal(self, message_text):
         pass
+
+    def is_probable_pump_time_announcement(self, message_text):
+
+        if message_text.strip().isdigit():
+            return True
+
+        prepared_text = message_text.replace(" ", "").lower()
+
