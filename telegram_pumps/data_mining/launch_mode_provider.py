bd89f2a4a18516453425ca337925da8c3e173efa
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-15 00:01:37 +0100

    Save session file blob to database upon successful login

--- telegram_pumps/data_mining/launch_mode_provider.py
+++ telegram_pumps/data_mining/launch_mode_provider.py
@@ -1,23 +1,51 @@
 from common.database.database_connection import create_db_connection
 
 
-def is_auth_code_available():
-    return __fetch_auth_data(False)
+def save_session_file():
+    print('saving session file')
+    db_connection = create_db_connection()
+    session_file_content = open('telegram.session', 'rb').read()
+    cursor = db_connection.cursor()
+    cursor.execute("DELETE FROM session_blob")
+    db_connection.commit()
+    cursor.execute("INSERT INTO session_blob(session_blob) VALUES (%s);",
+                   [session_file_content])
+    db_connection.commit()
+    db_connection.close()
+
+
+def save_phone_code_hash(hash):
+    db_connection = create_db_connection()
+    cursor = db_connection.cursor()
+    cursor.execute("DELETE FROM auth_code")
+    db_connection.commit()
+    cursor.execute("INSERT INTO auth_code(code, launch, code_hash) VALUES (%s, %s, %s);",
+                   [-1, False, hash])
+    db_connection.commit()
+    db_connection.close()
 
 
 def fetch_auth_code_from_db():
-    return __fetch_auth_data(True)
+    return __fetch_auth_data(param=0)
 
 
-def __fetch_auth_data(auth_code_requested):
+def is_auth_code_available():
+    return __fetch_auth_data(param=1)
+
+
+def fetch_phone_code_hash_from_db():
+    return __fetch_auth_data(param=2)
+
+
+def __fetch_auth_data(param):
     db_connection = create_db_connection()
     db_cursor = db_connection.cursor()
-    db_cursor.execute('SELECT code, launch FROM auth_code')
+    db_cursor.execute('SELECT code, launch, code_hash FROM auth_code')
     auth_data = db_cursor.fetchall()
     db_connection.close()
 
-    if auth_code_requested:
-        # a list containing one tuple
-        return auth_data[0][0]
+    # a list containing one tuple
+    if auth_data:
+        return auth_data[0][param]
     else:
-        return auth_data[0][1]
+        return None
--- telegram_pumps/data_mining/telegram_message_api_stream.py
+++ telegram_pumps/data_mining/telegram_message_api_stream.py
@@ -1,10 +1,13 @@
 from getpass import getpass
 
+import psycopg2
 from telethon import TelegramClient
 from telethon.errors import SessionPasswordNeededError
 from telethon.tl.types import UpdateNewChannelMessage
 
-from telegram_pumps.data_mining.launch_mode_provider import is_auth_code_available, fetch_auth_code_from_db
+from common.database.database_connection import create_db_connection
+from telegram_pumps.data_mining.launch_mode_provider import is_auth_code_available, fetch_auth_code_from_db, \
+    save_session_file, save_phone_code_hash, fetch_phone_code_hash_from_db
 from telegram_pumps.data_mining.message_handler import MessagesHandler
 
 user_phone = '+048698393574'
@@ -13,7 +16,7 @@ messages_handler = MessagesHandler()
 
 def _initialize_client():
     client = TelegramClient(
-        'examplesession',
+        'telegram',
         136572,
         '4c32af0a85f96a579d7e6f9f59fd7a77',
         proxy=None,
@@ -30,15 +33,20 @@ def _initialize_client():
             code_ok = False
             while not code_ok:
                 auth_code = fetch_auth_code_from_db()
+                phone_code_hash = fetch_phone_code_hash_from_db()
+                print(phone_code_hash)
                 try:
-                    code_ok = client.sign_in(user_phone, auth_code, phone_code_hash='87beda67ec8bc04b5d')
-                    print(auth_code)
+                    code_ok = client.sign_in(user_phone, auth_code, phone_code_hash=phone_code_hash)
                 except SessionPasswordNeededError:
                     password = getpass('Two step verification enabled. Please enter your password: ')
                     code_ok = client.sign_in(password=password)
+
+            save_session_file()
         else:
-            phone_code_hash = client.send_code_request(user_phone)
-            print(phone_code_hash)
+            resend_code_request = client.send_code_request(user_phone)
+            phone_code_hash = resend_code_request.phone_code_hash
+            print('Phone code hash', phone_code_hash)
+            save_phone_code_hash(phone_code_hash)
             print('A fresh auth code has been sent. Please update the value in db and deploy')
 
             # launch an infinite loop to prevent Heroku from restarting the script resulting in a telegram API ban
