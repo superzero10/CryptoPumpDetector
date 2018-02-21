c604437f2e9b3f634df70d630f71c9e80e92d39f
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-21 10:47:17 +0100

    Fix the pump time schedule finding regexp

--- telegram_pumps/pump_coin_extraction/signal_message_recognition.py
+++ telegram_pumps/pump_coin_extraction/signal_message_recognition.py
@@ -6,17 +6,21 @@ class PumpCoinExtractor:
     def extract_pump_signal(self, message_text):
         pass
 
-    def extract_minutes_to_pump(self, message):
-        message_text = message.lower()
-        regex = r'\d+[" "]*["min"|"минут"]+'
-        found_announcement = re.findall(regex, message_text)
-        return self.__extract_minutes_to_pump(found_announcement)
+    def extract_minutes_to_pump(self, message_text):
+        normalized_message_text = message_text.lower().strip()
 
-    def __extract_minutes_to_pump(self, found_substrings):
+        if normalized_message_text.isdigit():
+            return normalized_message_text  # some groups like countdown using messages which contain only the minutes
+        else:
+            return self.__find_minutes_to_pump(normalized_message_text)
+
+    def __find_minutes_to_pump(self, message):
+        regex = r'\d+[" "]*min|минут'
+        found_substrings = re.findall(regex, message)
         if not found_substrings:
             return None
         return ''.join((filter(str.isdigit, found_substrings[0])))
 
 
-print(PumpCoinExtractor().extract_minutes_to_pump("POZOR!!123    минутwegtwb"))
+print(PumpCoinExtractor().extract_minutes_to_pump("Two. Minutes. 30 minutes to the pump"))
 
