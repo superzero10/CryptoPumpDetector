38a8b6352f7c074c3e7175056e1dcc1f4a830671
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-21 16:28:24 +0100

    Trim text messages & remove excess whitespace

--- telegram_pumps/pump_coin_extraction/signal_message_recognition.py
+++ telegram_pumps/pump_coin_extraction/signal_message_recognition.py
@@ -8,15 +8,16 @@ class PumpCoinExtractor:
     _coin_extraction_pattern = r'(?<=\b\w)[ ]{1,}(?![ ]{0,}\w{2})'
 
     def extract_pump_signal(self, message_text):
-        print('MESSAGE BEFORE PROCESSING', message_text)
+        print('MESSAGE BEFORE PROCESSING:', message_text)
         stripped_message_text = self.__remove_special_characters(message_text)
         normalized_message_text = self.__normalize_message(stripped_message_text)
-        print('MESSAGE AFTER PROCESSING', normalized_message_text)
+        print('MESSAGE AFTER PROCESSING:', normalized_message_text)
 
     def __remove_special_characters(self, message):
-        message_without_emoji = re.sub(self._emoji_removing_pattern, '', message)
+        message_without_emoji = re.sub(self._emoji_removing_pattern, '', message).strip()
         message_without_special_characters = re.sub(self._letters_pattern, '', message_without_emoji)
-        return message_without_special_characters.replace('\n', ' ').replace('\r', '')
+        message_without_newlines = message_without_special_characters.replace('\n', ' ').replace('\r', '')
+        return re.sub('[ \t\n]+', ' ', message_without_newlines)
 
     def __normalize_message(self, message):
         return re.sub(self._coin_extraction_pattern, '', message)
