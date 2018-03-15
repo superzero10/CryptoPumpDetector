f54df34fe442cfc3282eee1157313295b8e2206a
Haydart <makowiecki.radoslaw@gmail.com>
2018-03-15 16:45:05 +0100

    Don't trade the pump coin if it was traded recently

--- telegram_pumps/trading/pump_trader.py
+++ telegram_pumps/trading/pump_trader.py
@@ -2,6 +2,7 @@ import threading
 import time
 from datetime import datetime
 from threading import Lock
+from time import time
 
 from common.exchange_services.cryptopia_service import CryptopiaService
 from common.exchange_services.yobit_service import YobitService
@@ -15,7 +16,7 @@ class PumpTrader:
     _cryptopia_service = CryptopiaService()
     _lock = Lock()
 
-    _recently_traded_coins = {}  # [(coin, exchange, timestamp)]
+    _recently_traded_coins = []  # [(coin, exchange, timestamp)]
 
     def trade_pumped_coin_if_viable(self, coin, exchange):
         print(datetime.time(datetime.now()), '||| PUMP DETECTED, coin:', coin, 'exchange:', exchange)
@@ -31,9 +32,12 @@ class PumpTrader:
                 self.__buy_coin(coin, exchange)
                 time.sleep(self._PUMP_COIN_HOLD_SECONDS)
                 self.__sell_coin(coin, exchange)
+            else:
+                print('|||||| COIN ', coin, 'WAS TRADED RECENTLY ON', exchange, 'ABORTING...')
 
     def __buy_coin(self, coin, exchange):
         print(datetime.time(datetime.now()), '||||||||| BOUGHT COIN', coin, 'ON EXCHANGE', exchange)
+        self._recently_traded_coins.append((coin, exchange, time.time()))
 
     def __sell_coin(self, coin, exchange):
         print(datetime.time(datetime.now()), '||||||||| SOLD COIN', coin, 'ON EXCHANGE', exchange)
