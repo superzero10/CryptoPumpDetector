2139639d3f073f9bedefa4b14591943d2add60ff
Haydart <makowiecki.radoslaw@gmail.com>
2018-03-15 23:59:05 +0100

    Fix threading flow in pump coin trading

--- telegram_pumps/trading/pump_trader.py
+++ telegram_pumps/trading/pump_trader.py
@@ -20,20 +20,21 @@ class PumpTrader:
 
     def trade_pumped_coin_if_viable(self, coin, exchange):
         print(datetime.time(datetime.now()), '||| PUMP DETECTED, coin:', coin, 'exchange:', exchange)
-        # create a new thread for each
-        trade_thread = threading.Thread(target=self.__trade_coin_if_viable(coin, exchange))
-        trade_thread.start()
 
-    def __trade_coin_if_viable(self, coin, exchange):
         with self._lock:
             if (coin, exchange) not in [(t_coin, t_exchange) for (t_coin, t_exchange, t_timestamp) in
                                         self._recently_traded_coins]:
-                # coin was not traded ever
-                self.__buy_coin(coin, exchange)
-                time.sleep(self._PUMP_COIN_HOLD_SECONDS)
-                self.__sell_coin(coin, exchange)
+                # if coin was not traded ever
+                # create a new thread for each trade process
+                trade_thread = threading.Thread(target=self.__trade_coin(coin, exchange))
+                trade_thread.start()
             else:
                 print('|||||| COIN ', coin, 'WAS TRADED RECENTLY ON', exchange, 'ABORTING...')
+
+    def __trade_coin(self, coin, exchange):
+        self.__buy_coin(coin, exchange)
+        time.sleep(self._PUMP_COIN_HOLD_SECONDS)
+        self.__sell_coin(coin, exchange)
 
     def __buy_coin(self, coin, exchange):
         print(datetime.time(datetime.now()), '||||||||| BOUGHT COIN', coin, 'ON EXCHANGE', exchange)
