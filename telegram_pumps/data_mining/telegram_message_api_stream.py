c9478dae219326fe55523494eddfc9957b37b9a0
Haydart <makowiecki.radoslaw@gmail.com>
2018-09-08 15:30:10 +0200

    Add session.txt file & improved session handling method

--- telegram_pumps/data_mining/telegram_message_api_stream.py
+++ telegram_pumps/data_mining/telegram_message_api_stream.py
@@ -13,32 +13,17 @@ messages_handler = MessageProcessor()
 
 
 def _initialize_client():
-    with open('session.txt', 'r') as file:
-        session = file.read()
     print_directory_files()
-
-    client = TelegramClient(
-        StringSession(session),
-        api_id=os.environ.get('API_ID'),
-        api_hash=os.environ.get('API_HASH'),
-        proxy=None
-    )
+    client = create_client_with_session(read_session_file())
 
     print(datetime.time(datetime.now()), 'INFO: Connecting to Telegram Servers...', end='', flush=True)
     client.connect()
 
-    client.disconnect()
-    if not client.is_user_authorized():
-        print(datetime.time(datetime.now()), 'Unauthorized user')
-
-
     print(datetime.time(datetime.now()), 'Client initialized, waiting for updates.')
     client.add_event_handler(_update_handler)
+
     with client.start():
-        with open('session.txt', 'w') as file:
-            saved_session_string = client.session.save()
-            file.write(saved_session_string)
-            print(saved_session_string)
+        write_session_file(client)
         print('(Press Ctrl+C to stop this)')
         client.run_until_disconnected()
 
@@ -47,6 +32,37 @@ async def _update_handler(update):
     print(update)
     if isinstance(update, UpdateNewChannelMessage):
         messages_handler.handle_channel_updates(update.message)  # whole Message object
+
+
+def read_session_file():
+    try:
+        with open('session.txt', 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
+
+
+def create_client_with_session(session):
+    if session is None:
+        # create a new session (mobile auth code required)
+        string_session = StringSession()
+    else:
+        # recreate session from existing file
+        string_session = StringSession(session)
+
+    return TelegramClient(
+        string_session,
+        api_id=os.environ.get('API_ID'),
+        api_hash=os.environ.get('API_HASH'),
+        proxy=None
+    )
+
+
+def write_session_file(client):
+    with open('session.txt', 'w') as file:
+        saved_session_string = client.session.save()
+        file.write(saved_session_string)
+        print(saved_session_string)
 
 
 if __name__ == '__main__':
